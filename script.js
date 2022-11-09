33c7b416e421c7be58315ee81f6414b7a38b3dce
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-11-09 16:05:30 +0100

    Optimised code and fixed some bugs

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const Player = (name, tile) => {
     const getName = () => name;
     const getTile = () => tile;
     const makeMove = (row, column) => {
-        if (Gameboard.gameBoard[row][column] == '') {
+        if (Gameboard.gameBoard[row][column] == "") {
             Gameboard.gameBoard[row][column] = getTile();
             return true;
         } 
@@ -83,7 +83,7 @@ const Game = (() => {
                 alert(`${turn.getName()} won!`);
             } else {
                 if (checkTie()) {
-                    alert(`It's a tie!`);
+                    alert("It's a draw");
                 }
                 else {
                     doTurn();
